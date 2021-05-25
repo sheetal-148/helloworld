e830d8ce0eb814ff07444f164127e6c3ddf490cd
sheetal <shindeshital393@gmail.com>
2021-05-25 23:29:36 +0530

    first commit

---
+++ src/firstpkg/myapp.java
@@ -0,0 +1,14 @@
+package firstpkg;
+
+public class myapp {
+
+	public myapp() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("Hello World");
+	}
+
+}
